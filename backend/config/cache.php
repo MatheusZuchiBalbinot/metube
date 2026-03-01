92a539f411ef79196138635189ab42caf8441e24
MatheusZuchiBalbinot <matheusbalbinotzuchi@gmail.com>
2026-02-28 23:13:41 -0300

    refactor(cache): use fixed vidsum prefix instead of dynamic slug

--- backend/config/cache.php
+++ backend/config/cache.php
@@ -111,7 +111,5 @@ return [
     | that reason, you may prefix every cache key to avoid collisions.
     |
     */
-
-    'prefix' => env('CACHE_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-cache-'),
-
+    'prefix' => env('CACHE_PREFIX', 'vidsum'),
 ];
