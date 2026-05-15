ec4049269c2df9dc8daa6f13c23aa4f1486eb5fd
MatheusZuchiBalbinot <matheusbalbinotzuchi@gmail.com>
2026-05-15 19:05:58 -0300

    refactor(user): optimize history queries with SQL aggregation

---
+++ backend/app/Http/Resources/WatchHistoryResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class WatchHistoryResource extends JsonResource
+{
+    /** @return array<string, mixed> */
+    public function toArray(Request $request): array
+    {
+        return [
+            'vuid' => $this->video->vuid,
+            'watched_at' => $this->watched_at->toIso8601String(),
+        ];
+    }
+}
