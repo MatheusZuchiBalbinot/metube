15a8b0619c95f3537f9c9b62c8e486887eab1abf
MatheusZuchiBalbinot <matheusbalbinotzuchi@gmail.com>
2026-06-04 16:16:39 -0300

    refactor: update hooks to use extracted Redux selectors

--- frontend/src/hooks/useSubscription.ts
+++ frontend/src/hooks/useSubscription.ts
@@ -1,10 +1,11 @@
 import { useAppDispatch, useAppSelector } from '@store';
-import { subscriptionActions, selectSubscribedSet } from '@store/subscriptionSlice';
+import { subscriptionActions } from '@store/subscriptionSlice';
+import { selectSubscribedChannelIds, selectSubscribedSet } from '@store/subscriptionSelectors';
 import type { ChannelId } from '@models';
 
 export function useSubscription() {
     const dispatch = useAppDispatch();
-    const subscribedChannelIds = useAppSelector(s => s.subscription.subscribedChannelIds);
+    const subscribedChannelIds = useAppSelector(selectSubscribedChannelIds);
     const subscribedSet = useAppSelector(selectSubscribedSet);
 
     function toggleSubscription(channelId: ChannelId) {
